c209f6a6ec957b0664af4a5d861487e7a98019ef
KunigiMohan <kmohanr5@gmail.com>
2021-12-01 22:00:07 +0530

    Added : Converting given number to word.

---
+++ NumbertoWord.js
@@ -0,0 +1,35 @@
+const ps = require("prompt-sync");
+const prompt = ps();
+
+let number = prompt("Enter number to display number in word : ");
+
+if(number==1){
+    console.log("One");
+}
+else if(number ==2){
+    console.log("Two");
+}
+else if(number ==3){
+    console.log("Three");
+}
+else if(number ==4){
+    console.log("Four");
+}
+else if(number ==5){
+    console.log("Five");
+}
+else if(number ==6){
+    console.log("Six");
+}
+else if(number ==7){
+    console.log("Seven");
+}
+else if(number ==8){
+    console.log("Eight");
+}
+else if(number ==9){
+    console.log("Nine");
+}
+else if(number ==10){
+    console.log("Ten");
+}
