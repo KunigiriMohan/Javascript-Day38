0f90e96c516bdeaf73692600b828fd8ab485f7ac
KunigiMohan <kmohanr5@gmail.com>
2021-12-01 13:59:24 +0530

    Added : Length conversion from feets to meters and meters to feet.

---
+++ Unitconversion.jas.js
@@ -0,0 +1,18 @@
+//  (A)
+
+let inch=12;
+let feet=(42/12);
+
+//  (B)
+
+let length = 60;
+let breadth = 40;
+
+let length_in_meters=60*(0.3048);
+let breadth_in_meters=40*(0.3048);
+
+let area_in_Meters=length_in_meters*breadth;
+
+//  (C)
+
+let area_of_25_such_Plots=25*area_in_Meters;
