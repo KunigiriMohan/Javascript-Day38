ecf47664565802c43b093ed890f98e89d1d4a6b9
KunigiMohan <kmohanr5@gmail.com>
2021-12-01 14:02:31 +0530

    Added : Generating 10 random numbers, rolling dice and finding sum of two dices.

---
+++ randomnumber.js
@@ -0,0 +1,13 @@
+let ran = Math.floor(Math.random()*10);
+console.log("Random number below 10 : "+ran);
+
+let ran2=Math.floor(Math.random()*6)+1;
+console.log("Dice value : "+ran2);
+
+let dice1=Math.floor(Math.random()*6)+1;
+console.log("Dice1 value : "+dice1);
+
+let dice2=Math.floor(Math.random()*6)+1;
+console.log("Dice2 value : "+dice2);
+
+console.log("Sum of Dices value : "+(dice1+dice2));
