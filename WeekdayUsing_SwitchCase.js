552977794ab2883a458aa8d1d828cd6adb11a5c3
KunigiMohan <kmohanr5@gmail.com>
2021-12-02 07:17:17 +0530

    Added : Taking day number input and printing Day using Switch Case.

---
+++ WeekdayUsing_SwitchCase.js
@@ -0,0 +1,31 @@
+const ps =require("prompt-sync");
+const prompt= ps();
+
+let dayNumber= prompt("Enter Day Number : ");
+
+switch(dayNumber){
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Enter proper value");
+        break;
+}
